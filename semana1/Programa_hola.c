cf586bde2340cbb189b6c04e490f1ede88c9e96d
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-08-08 09:25:41 -0500

    Cree un programa.

--- semana1/Programa_hola.c
+++ semana1/Programa_hola.c
@@ -1,5 +1,5 @@
 /*
-	Esto e sun comentario. Me sirve para escribir informacion relevante del programa que estoy haciendo. Por ejemplo:
+	Esto es un comentario. Me sirve para escribir informacion relevante del programa que estoy haciendo. Por ejemplo:
 	Este programa imprime la típica frase "Que paso jeje"
-	Creado el 06/08/2018 por emdajhuda.
+	Creado el 06/08/2018 por emdajhuda. Salu2.
 */
---
+++ semana1/programa1.c
@@ -0,0 +1,13 @@
+/*Creado el 8 de agosto del 2018 por Juda Rodriguez (emdajhuda)*/
+
+#include<stdio.h>
+int.main()
+{
+
+ printf("Jsjsjs, Qui onda? \n");
+ printf("¿Qué andas haciendo por aca? \n");
+
+}
+
+/* Termine de escribir el programiux*/ 
+
