875c1f1fbd58d9b22de51d4407a6b83ce9b03ddf
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-08-15 08:38:16 -0500

    Compile el programa de la edad (15/8/2018)

---
+++ semana2/prorgama_edad.c
@@ -0,0 +1,18 @@
+/*creado el 12 de agosto del 2018 por Judá Rodriguez(emdajhuda) y modificado el 15 de agosto del 2018*/
+
+#include<stdio.h>
+
+int main()
+{
+
+int edad;
+
+	printf("Hola Judá \n");
+	printf("Este es el segundo programa que haces \n");
+	printf("Introduce tu edad: \n");
+	scanf("%i", &edad);
+	printf("\nTu edad es: %i años \n");
+return(0);
+
+}
+/*Termine de escribir el programa*/
