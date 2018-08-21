1d8c16e65d45860cd58d2e8f592e20663e550ae3
emdajhuda <rodriguezne2018@licifug.ugto.mx>
2018-08-21 10:44:12 -0500

    Termine el ejercicio3

--- semana3/ejercicio3.c
+++ semana3/ejercicio3.c
@@ -1,4 +1,4 @@
-/*Este programa va a convertir coordenadas cartesianas a coordenadas esfericas*/
+/*Este programa va a convertir coordenadas cartesianas a coordenadas esfericas fue creado el 20/08/2018 por Judá Rodríguez(emdajhuda) y terminado el 21/08/2018*/
 #include <stdio.h>
 #include <math.h>
 
@@ -6,24 +6,43 @@ int main()
 {
 float x1, x2, y1, y2, z1, z2, r1, r2, theta1, theta2, fi1, fi2;
 
-	printf("Este programa transforma coordenadas cartesianas a coordenadas esfericas \n");
-	printf("Proporciona las coordenadas cartesianas que deseas transformar \n");
+	printf("Este programa transforma coordenadas cartesianas a coordenadas esfericas y viceversa \n");
+	printf("Proporciona las coordenadas cartesianas que deseas transformar: \n");
 	
-	printf("x: \n");
-	scanf("%f\n", &x1);
-	printf("y: \n");
-	scanf("%f\n", &y1);
-	printf("z: \n");
-	scanf("%f\n", &z1);
+	printf("x: ");
+	scanf("%f", &x1);
+	printf("y: ");
+	scanf("%f", &y1);
+	printf("z: ");
+	scanf("%f", &z1);
 	
-	r1=sqrt(pow(x1,2)+pow(y1,2)+pow(z1,2));
-	theta1=acos(z1/r1);
+	(r1=(sqrt((pow(x1,2))+(pow(y1,2))+(pow(z1,2)))));
+;
+	theta1=atan((sqrt(pow(x1,2)+pow(y1,2)))/z1);
 	theta1=theta1*180/3.1415926;
 	fi1=atan(y1/x1);
 	fi1=fi1*180/3.1415926;
-//CAmbiar las ecuaciones a las de http://laplace.us.es/wiki/index.php/Relaci%C3%B3n_entre_los_distintos_sistemas_de_coordenadas
 
-	printf("r=%f\n", r1);
-	printf("theta=%f°\n", theta1);
-	printf("fi=%f°\n", fi1);
+	printf("r=	%f\n", r1);
+	printf("theta=	%f°\n", theta1);
+	printf("fi=	%f°\n", fi1);
+	printf("Proporciona las coordenadas esfericas que deseas transformar: \n");
+	
+	printf("r: ");
+	scanf("%f", &r2);
+	printf("theta: ");
+	scanf("%f", &theta2);
+	printf("fi: ");
+	scanf("%f", &fi2);
+	
+	theta2=theta2*3.1415926/180;
+	fi2=fi2*3.1415926/180;
+
+	x2=r2*sin(theta2)*cos(fi2);
+	y2=r2*sin(theta2)*sin(fi2);
+	z2=r2*cos(theta2);
+	
+	printf("x=	%f\n", x2);
+	printf("y=	%f\n", y2);
+	printf("z=	%f\n", z2);
 }
