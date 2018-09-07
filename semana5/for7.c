7b95a8654e0dace9530ed8c1067d5fdeb38da7e7
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-09-07 16:04:18 -0500

    Añadi los diagramas de  las actividades y modifique for7

--- semana5/for7.c
+++ semana5/for7.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
+#include <math.h>
 
 int main ()
 
 {
-	int d, dj, j, b;
+	int d, b, j=0, r;
 
 	printf("Este programa transforma números decimales a números binarios \n");
 	printf("Inserta el número decimal que deseas convertir a binario \n");
-	scanf("%i", &dj);
-
-	printf("Tu número en binario esta de abajo hacia arriba:\n");
-
-for ((j=0); (j=dj); (j++))
-	{
-	d=dj
-	b=d%2;
-	printf("%i \n", b);
-	
+	scanf("%i", &d);
+while (r!=0)
+	{	
+	b=d%(2^j);
+	if (b==0)
+		{
+		printf("%i ", b);
+		}
+	else 
+		{
+		printf("1 ");
+		}
+	r=d;
+	j++;
 	}
 
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
