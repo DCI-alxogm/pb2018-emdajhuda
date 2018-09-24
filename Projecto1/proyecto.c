9d89f9145073f9b8834ad321e3873212d3d73e82
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-09-24 09:52:55 -0500

    Empece mi proyecto.

---
+++ Projecto1/proyecto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+FILE* data;
+FILE* mercurio;
+FILE*
+int te;
+float h;
+a=te/h;
+double m1, m2, m3, m4, m5, m6, m7, m8;
+double x1[a], x2[a], x3[a], x4[a], x5[a], x6[a], x7[a], x8[a];
+double y1[a], y2[a], y3[a], y4[a], y5[a], y6[a], y7[a], y8[a];
+double z1[a], z2[a], z3[a], z4[a], z5[a], z6[a], z7[a], z8[a];
+double vx1[a], vx2[a], vx3[a], vx4[a], vx5[a], vx6[a], vx7[a], vx8[a];
+double vy1[a], vy2[a], vy3[a], vy4[a], vy5[a], vy6[a], vy7[a], vy8[a];
+double vz1[a], vz2[a], vz3[a], vz4[a], vz5[a], vz6[a], vz7[a], vz8[a];
+
+}
+return 0;
