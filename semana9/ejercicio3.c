39f8c14be8dae920d968a2234a482f53a06a54f2
emdajhuda <rodriguezne2018@licifug.ugto.mx>
2018-10-11 12:55:07 -0500

    Termine el programa ejercicio3 y empece el programa ejercicio4.

--- semana9/ejercicio3.c
+++ semana9/ejercicio3.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 
+void cuadrado(float x);
+
+int main()
+{
+float x, x2;
+printf("Introduce el número del que quieras calculcar su cuadrado:");
+scanf("%f", &x);
+cuadrado(x);
+return 0;
+}
+void cuadrado(float x)
+{
+float x2;
+x2=x*x;
+printf("EL cuadrado de %f es %f\n", x, x2);
+}
+
+
 
---
+++ semana9/ejercicio4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float cuadrado();
+int main()
+{
+float x, x2;
+cuadrado();
+printf("EL cuadrado de %f es %f\n", x, x2);
+}
+float cuadrado()
+{
+float x, x2;
+printf("Introduce el número del que quieras calculcar su cuadrado:");
+scanf("%f", &x);
+
+return x2=x*x;
+}
