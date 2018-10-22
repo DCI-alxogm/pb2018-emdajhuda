14d8076eb12ae0909e843ed5bc1fc577975566b9
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-10-22 13:28:14 -0500

    Progrese en mis proyecto2.

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main(int arg, char *argt[])
 {
 
-	char *data;
+	char *datos;
 	char *resultados;
-	int N, n;
+	int N, n, i, j;
 
 	float *T[i][j], l1, l2, l3, l4;
 
@@ -20,23 +18,74 @@ int main(int arg, char *argt[])
 	if(arg==3)
 	{
 
-	data = fopen(data, "r");
+	datos=argt[1];
+	resultados=argt[2];
+	data = fopen(datos, "r");
 
 	fscanf(data, "%f%f%f%f%f", &l1, &l2, &l3, &l4, &n);
 
+	fclose(data);
 	N=n*n;
 
-	T[i][j]=(float*) calloc(N, sizeof(int));
+	T[][]=(float*) calloc(N, sizeof(int));
 
-	for (i=1; i<(n-1); i++)
+	for (i=0; i<n; i++)
 		{
-		for (j=1; j<(n-1); j++)
+		for (j=0; j<n; j++)
 			{
-			T[i][j]=0;
+			*T[i][j]=0;
 			}
 		}
-	for
-	free(T[i][j]);
+
+	for (i=0; i<n; i++)
+		{
+		for (j=0; j<n; j++)
+			{
+			if(i==0)
+				{
+				if(j==0)
+					{
+					*T[i][j]=(l1+l3)/4;
+					}
+				else if(j==n)
+					{
+					*T[i][j]=(l1+l4)/4;
+					}
+				else
+					{
+					*T[i][j]=(l1)/4;
+					}
+				}
+			if(i==n)
+				{
+				if(j==0)
+					{
+					*T[i][j]=(l2+l3)/4;
+					}
+				else if(j==n)
+					{
+					*T[i][j]=(l2+l4)/4;
+					}
+				else
+					{
+					*T[i][j]=(l2)/4;
+					}
+				}
+			}
+		}
+	for (i=0; i<n; i++)
+		{
+
+		out = fopen(resultados, "w");
+		for (j=0; j<n; j++)
+			{
+		fprintf(out, "%f", *T[i][j]);
+
+		fclose(out);
+			}
+		}
+
+	free(T[][]);
 	}
 	else if(arg>3)
 	{
@@ -48,3 +97,4 @@ int main(int arg, char *argt[])
 	}
 
 return 0;
+}
