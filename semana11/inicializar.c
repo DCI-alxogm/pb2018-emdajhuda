f32388dd1a478b61493a5d7d494a923b8575850d
Rodriguez Nachez Emmanuel Juda <rodriguezne2018@dci25-177.ugto.mx>
2018-10-29 17:25:44 -0600

    Termine de graficar.

--- semana11/inicializar.c
+++ semana11/inicializar.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+//La función inicializar inicializa los datos de la matriz, las de las temperaturas fijas, hasta las que van en el centro de la matriz.
 
-
+//Para esta funcion se necesitan los datos de las temperaturas y del numero de puntos por lado en la matriz.
 void inicializar(float l1, float l2, float l3, float l4, int n)
 
 {
+//Aqui se declaran las variables de los contadores para el for y tambien se declara la variable de la matriz.
 	int i, j;
 	float T[n+2][n+2];
-
+//indico que se usara un archivo al que me referire como out, este archivo sera para imprimir los valores iniciales de la matriz en un archivo.
 	FILE* out;
-
+//Aqui inicializo todos los puntos de la matriz a 0.
 	for (i=0; i<(n+2); i++)
 		{
 		for (j=0; j<(n+2); j++)
@@ -17,26 +19,29 @@ void inicializar(float l1, float l2, float l3, float l4, int n)
 			T[i][j]=0;
 			}
 		}
-
+//Y aqui se inicializan los valores de las columnas y filas adyacentes a la placa que tendran las temperaturas fijas impuestas por el usuario.
+//Para todo valor de i=0 se le ingresara la temperatura 1.
 	for (j=1; j<(n+1); j++)
 		{
 		T[0][j]=l1;
 		}
+//Para todo valor de i=n+1 se le ingresara la temperatura 2.
 	for (j=1; j<(n+1); j++)
 		{
 		T[(n+1)][j]=l2;
 		}
-
+//Para todo valor de j=0 se le ingresara la temperatura 4.
 	for (i=1; i<(n+1); i++)
 		{
 		T[i][0]=l4;
 		}
+//Para todo valor de j=n+1 se le ingresara la temperatura 3.
 	for (i=1; i<(n+1); i++)
 		{
 		T[i][(n+1)]=l3;
 		}
 
-
+//Aqui se abre el archivo 0placa.txt y se imprime dentro los valores iniciales de la matriz para que sean leidos en la función main.
 	out = fopen("0placa.txt", "w");
 
 	for (j=0; j<(n+2); j++)
--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -2,32 +2,37 @@
 #include <stdio.h>
 //Aqui indico que voy a usar unar variable para inicializar la matriz.
 void inicializar(float l1, float l2, float l3, float l4, int n);
-//Para la función
+//Para la función main daremos desde la terminal el archivo a ejecutar y el nombre del archivo donde estan los datos iniciales.
 int main(int arg, char *argt[])
 {
-
+//Declaramos la variable para escanear los datos.
 	char *datos;
-	char placa[20];
+//Aqui declaré la variable con la que ire guardando los nombres de los archivos para imprimir.
+	char placa[60];
+//Declaré las variables del tamaño de la matriz, temperaturas, contadores para los ciclos y epsilon.
 	int N, n, i, j, c, cw=0, p=1;
 
 	float l1, l2, l3, l4, em=1, e=0.001;
 
+//Se declaró las variables de donde se sacaran los datos y de donde se abriran.
 	FILE* data;
 	FILE* out;
-
+//Le muestra el programa que se esta ejecutando al usuario.
 	printf("El programa que estas ejecutando es: %s\n", argt[0]);
 
+//Le asigna a el segundo termino ingresado en la terminal al archivo que se abrira para escanear los datos.
 	datos=argt[1];
-
+//Se leen los datos en el siguiente orden, primero temperaturas y despues el numero de puntos por lado en la matriz.
 	data = fopen(datos, "r");
 
 	fscanf(data, "%f %f %f %f %i", &l1, &l2, &l3, &l4, &n);
 
 	fclose(data);
-
+//Aqui calculo el numero de puntos en la matriz para calcular el numero de epsilon en la matriz.
 	N=n*n;
-
+//Se declaran las matrices con tamaño n+2, para que agregue las columnas y filas con temperatura fija, ademas se declara un array para los valores del epsilon.
 	float T[n+2][n+2], Tv[n+2][n+2], ei[N];
+//Aqui se inicializan los puntos en la matriz temperatura vieja a 0.
 		for (i=0; i<(n+2); i++)
 		{
 		for (j=0; j<(n+2); j++)
@@ -36,10 +41,16 @@ int main(int arg, char *argt[])
 			}
 		}
 
+//Este if es para que se ejecute el programa solo si en la terminal se dan los nombres del programa a ejecutar y el archivo del que se sacaran los datos iniciales.
+
 	if(arg==2)
 	{
 
+//Con esta función se inicializan los puntos en la matriz.
+
 	inicializar(l1, l2, l3, l4, n);
+
+//Se escanea la matriz con los puntos iniciales.
 
 	out = fopen("0placa.txt", "r");
 
@@ -53,32 +64,40 @@ int main(int arg, char *argt[])
 
 	fclose(out);
 
+//Este ciclo while se repite maximo 3000 veces o hasta que alcance el equilibrio, para mi el equilibrio se alcanza cuando el epsilon mas grande es igual a 0.001 (dato que indique al inicializar la variable).
+
 	while (cw<3000 && em>e)
 	{
+//inicializo el contador c a 0 para los epsilon.
 	c=0;
+//En este ciclo for calculo el promedio de las temperaturas en las celdas adyacentes y calculo su epsilon.
 	for (j=1; j<(n+1); j++)
 		{
 		for (i=1; i<(n+1); i++)
 			{
 			T[i][j]=(T[i-1][j]+T[i+1][j]+T[i][j+1]+T[i][j-1])/4;
 			ei[c]=((T[i][j]-Tv[i][j])/T[i][j]);
+			//Aqui aumento el contador del epsilon una unidad para el siguiente calculo de promedio de temperatura.
 			c++;
+			//igualo la temperatura que acabo de sacar con la vieja, asi puedo calcular el siguiente epsilon.
 			Tv[i][j]=T[i][j];
 			}
 		}
+//Aqui encuentro el epsilon maximo en la matriz.
 	for(c=0; c<N; c++)
 		{
+//Escanea los epsilon uno por uno y si el epsilon individual es mayor al epsilon maximo, el epsilon maximo obtendra el valor del epsilon individuial leido.
 			if (em < ei[c])
 			{
 				em = ei[c];
 			}
 		}
-	
+//Aqui le asigno un diferente nombre a cada archivo en el que se imprimira.
 	sprintf(placa, "%dplaca.txt", p);
-
+//Como son muchos calculos solo imprimire cada 50, asi maximo tendre 60 archivos de texto.
 	if(cw%50==0)
 		{
-
+//Aqui imprimo la matriz y aumento una unidad en el contador de impresiones, asi cuando imprima los nombres de los archivos los escribira de uno en uno y si utilizo el contador del ciclo while me escribira los nombres de los archivos con saltos muy grandes en numero.
 		out = fopen(placa, "w");
 
 		for (j=1; j<(n+1); j++)
@@ -91,11 +110,11 @@ int main(int arg, char *argt[])
 			}
 		fclose(out);
 		p++;
-		}		
-
+		}
+//Le agrego una unidad al contador del ciclo while para que se pueda detener si llega a hacer el ciclo 3000 veces.
 	cw++;
 	}
-
+//Esto hace que el programa se detenga si no ingresamos los argumentos suficientes en la terminal o exedemosel numero de argumentos.
 	}
 	else if(arg>2)
 	{
@@ -105,6 +124,7 @@ int main(int arg, char *argt[])
 	{
 		printf("Se requiere de al menos 2 argumentos.\n");
 	}
+//Se cierra el programa
 
 return 0;
 }
